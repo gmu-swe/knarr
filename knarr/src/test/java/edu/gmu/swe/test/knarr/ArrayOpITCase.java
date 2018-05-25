1bec7ae25c6575fb9e44924a59c5d499daf1e536
Luis Pina <luis@luispina.me>
2018-05-25 18:04:49 -0400

    Added array test

---
+++ knarr/src/test/java/edu/gmu/swe/test/knarr/ArrayOpITCase.java
@@ -0,0 +1,37 @@
+package edu.gmu.swe.test.knarr;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.gmu.swe.knarr.runtime.Symbolicator;
+
+public class ArrayOpITCase {
+
+//	@Test
+	public void testArrayWrite() throws Exception {
+		byte tainted[] = new byte[1];
+
+		tainted[0] = Symbolicator.symbolic("a", (byte)'a');
+		
+		if (tainted[0] == 'a')
+			tainted[0] = 'b';
+
+		assertNotEquals(tainted[0], 'a');
+
+		assertFalse(Symbolicator.dumpConstraints().isEmpty());
+	}
+	@Test
+	public void testArrayIndex(){
+		int idx = Symbolicator.symbolic("Aindex",5);
+		int[] ar = new int[100];
+		ar[5] = 10;
+		assertEquals(10,ar[idx]);
+		assertFalse(Symbolicator.dumpConstraints().isEmpty());
+
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new ArrayOpITCase().testArrayWrite();
+	}
+}
