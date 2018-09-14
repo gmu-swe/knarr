c58acbd874a0e4a215f66f368997d161d89b300d
Luis Pina <luis@luispina.me>
2018-09-14 14:14:17 -0400

    Keep input with max contraints per variable

--- knarr-server/src/main/java/edu/gmu/swe/knarr/server/concolic/picker/MaxConstraintsPicker.java
+++ knarr-server/src/main/java/edu/gmu/swe/knarr/server/concolic/picker/MaxConstraintsPicker.java
@@ -2,26 +2,67 @@ package edu.gmu.swe.knarr.server.concolic.picker;
 
 import edu.gmu.swe.knarr.server.Canonizer;
 import edu.gmu.swe.knarr.server.concolic.Input;
+import za.ac.sun.cs.green.expr.Expression;
+import za.ac.sun.cs.green.expr.Variable;
+import za.ac.sun.cs.green.expr.Visitor;
+import za.ac.sun.cs.green.expr.VisitorException;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.TreeSet;
 
 public class MaxConstraintsPicker extends Picker {
 
-    int max = 0;
+    private int max = 0;
+    private Input maxIn = null;
+    private HashMap<Variable, Integer> maxVars = new HashMap<>();
+    private HashMap<Variable, Input>   maxIns = new HashMap<>();
 
     @Override
     public Input doPickInput() {
         return (((TreeSet<Input>)inCirculation)).last();
     }
 
+    public static boolean refersVar(Variable v, Expression e) {
+        FindVariableVisitor visitor = new FindVariableVisitor(v);
+
+        try {
+            e.accept(visitor);
+        } catch (VisitorException e1) {
+            throw new Error(e1);
+        }
+
+        return visitor.found;
+    }
+
     @Override
     protected boolean shouldSaveInput(Input in) {
         int n = countConstraints(in.constraints);
 
+        for (Variable v : in.constraints.getVariables()) {
+            HashSet<Expression> exps = in.constraints.getCanonical().get(v.toString());
+
+            if (exps == null)
+                continue;
+
+            int vars = exps.size();
+
+            for (Expression e : in.constraints.getNotCanonical())
+                if (refersVar(v, e))
+                    vars++;
+
+            Integer count = maxVars.get(v);
+            if (count == null || vars > count) {
+                maxVars.put(v, vars);
+                maxIns.put(v, in);
+            }
+        }
+
         if (n > max) {
             max = n;
+            maxIn = in;
             return true;
         }
 
@@ -50,5 +91,28 @@ public class MaxConstraintsPicker extends Picker {
             n += cc.size();
 
         return n;
+    }
+
+    public Input getMaxInput(Variable v) {
+        return maxIns.get(v);
+    }
+
+    public Input getMaxInput() {
+        return maxIn;
+    }
+
+    private static class FindVariableVisitor extends Visitor {
+        private Variable toFind;
+        private boolean found = false;
+
+        public FindVariableVisitor(Variable toFind) {
+            this.toFind = toFind;
+        }
+
+        @Override
+        public void preVisit(Variable variable) throws VisitorException {
+            if (variable.equals(toFind))
+                found = true;
+        }
     }
 }
