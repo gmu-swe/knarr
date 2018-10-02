25d9c821e923c9d05232ceede8efe459271191b1
Luis Pina <luis@luispina.me>
2018-10-02 16:31:29 -0400

    Special handling of strings protected with env variable

--- knarr/src/main/java/edu/gmu/swe/knarr/runtime/StringTagFactory.java
+++ knarr/src/main/java/edu/gmu/swe/knarr/runtime/StringTagFactory.java
@@ -37,7 +37,7 @@ public class StringTagFactory extends ClassVisitor implements Opcodes {
 	@Override
 	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
 		super.visit(version, access, name, signature, superName, interfaces);
-		enabled = name.equals(stringType.getInternalName());
+		enabled = name.equals(stringType.getInternalName()) && (System.getProperty("specialStrings") != null);
 	}
 
 	@Override
