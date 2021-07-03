b21623294607ba678b5a95511f69b19065f0d783
Jonathan Bell <jon@jonbell.net>
2021-07-03 19:39:47 -0400

    OK I sure hope that this fixes gitlab deploy

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,4 +5,8 @@ TRAVIS_BUILD_DIR=`pwd`
 base64 -d $DEPLOY_KEY > $TRAVIS_BUILD_DIR/deployment/signingkey.asc
 gpg --batch --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import $TRAVIS_BUILD_DIR/deployment/signingkey.asc;
 gpg --batch --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/secring.gpg --no-default-keyring --import $TRAVIS_BUILD_DIR/deployment/signingkey.asc;
+cd $TRAVIS_BUILD_DIR/knarr;
+mvn -DskipTests deploy --settings $TRAVIS_BUILD_DIR/deployment/settings.xml -Dgpg.keyname=77787D71ED65A50488D41B82E876C482DFB8D3EB -Dgpg.passphrase=$DEPLOY_KEY_PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/secring.gpg
+
+cd $TRAVIS_BUILD_DIR/knarr-server;
 mvn -DskipTests deploy --settings $TRAVIS_BUILD_DIR/deployment/settings.xml -Dgpg.keyname=77787D71ED65A50488D41B82E876C482DFB8D3EB -Dgpg.passphrase=$DEPLOY_KEY_PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/secring.gpg
